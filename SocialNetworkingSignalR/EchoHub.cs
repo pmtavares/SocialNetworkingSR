17170ebd1c2fb7162e4a3697656ea119933cd180
pmtavares <pmauriciotavares@yahoo.com.br>
2018-03-26 22:24:24 +0100

    Tested SignalR

--- SocialNetworkingSignalR/EchoHub.cs
+++ SocialNetworkingSignalR/EchoHub.cs
@@ -4,15 +4,21 @@ using System.Linq;
 using System.Web;
 using Microsoft.AspNet.SignalR;
 using Microsoft.AspNet.SignalR.Hubs;
+using System.Diagnostics;
 
 namespace SocialNetworkingSignalR
 {
     [HubName("echo")]
     public class EchoHub : Hub
     {
-        public void Hello()
+        public void Hello(string message)
         {
-            Clients.All.hello();
+            //Clients.All.hello();
+            Trace.WriteLine(message);
+
+            var clients = Clients.All;
+
+            clients.test("This is a test");
         }
     }
 }
